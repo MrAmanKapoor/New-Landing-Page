1a1538961c116e241a34dd1d1f038368c85d17b0
Aman Kapoor <amankapoor140@gmail.com>
2022-06-02 14:22:13 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+function menu() {
+    let s = document.getElementById('menu');
+    s.style.display = "block";
+}
+
+function clos() {
+    let x = document.getElementById('menu');
+    x.style.display = "none";
+}
